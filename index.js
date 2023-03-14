6ec0ddb63589a12c011423a156720be41a8190a1
Atai Nail <81722547+atainail@users.noreply.github.com>
2023-03-14 16:38:42 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -7,25 +7,26 @@ $('button').click(function() {
 $('.navigation-import').load("nav.txt");
 
 
-
-
-
-
-
-
 //imports and puts all photos from folder into an array 
 
 let weddingPicsArr = [];
+let headShotsArr = [];
+
+
+
+
 
 function importPictures(totalNumOfPhotos, arrName, folderName, photoType){
     for(let i = 1; i < totalNumOfPhotos + 1; i++){
-        arrName[i] = `<img class="reveal" src="${folderName}/${photoType}${i}.jpg">`;
-        document.querySelector(".wedding-photos").innerHTML +=  `<img class="reveal" src="${folderName}/${photoType}${i}.jpg">`;   
-         
+        arrName[i] = `<img class="reveal " src="${folderName}/${photoType}${i}.jpg">`;
+        document.querySelector('.wedding-photos').innerHTML +=  `<img class="reveal " src="${folderName}/${photoType}${i}.jpg">`;   
+        // document.querySelector('.head-shots').innerHTML +=  `<img class="reveal photos" src="${folderName}/${photoType}${i}.jpg">`;   
+        console.log(headShotsArr)
     }
 }
-importPictures(17, weddingPicsArr, 'wedding-pics', 'wedding');
 
+    importPictures(17, weddingPicsArr, 'wedding-pics', 'wedding');
+    // importPictures(7 , headShotsArr  , 'headshots'   , 'head'   ); 
 
 
 function reveal() {
@@ -42,8 +43,6 @@ function reveal() {
     }
   }
   window.addEventListener("scroll", reveal);
-
-
 
   window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
