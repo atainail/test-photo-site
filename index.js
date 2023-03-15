cf319656af5b2c15d5355d8129e4d5d9a990bd60
Atai Nail <81722547+atainail@users.noreply.github.com>
2023-03-15 19:18:30 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ else if (title.includes('Headshots')) {
 
 
 
-
 //will have the photos fade in
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
@@ -64,4 +63,22 @@ window.addEventListener("scroll", (event) => {
     }
 });
 
+
+//displays the arrow to skip back to the top of the page
+window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    let pageHeight = document.body.scrollHeight;
+    if (scroll < pageHeight/6) {
+        document.querySelector(".top-of-page").innerHTML = ""; 
+    } else if (scroll > pageHeight/6){
+        document.querySelector(".top-of-page").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-short" viewBox="0 0 16 16">'
+      +  '<path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>'
+      + '</svg>'
+    }
+});
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
 
